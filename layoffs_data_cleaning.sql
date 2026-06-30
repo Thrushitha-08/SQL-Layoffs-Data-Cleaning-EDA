73462bb1ad57d69d06ccd998c4c5be1598b24457
Thrushitha-08 <nomulathrushitha6065@gmail.com>
2026-06-30 23:16:59 +0530

    Clean and transform layoffs data in SQL
    This SQL script performs data cleaning and transformation on the layoffs dataset, including standardizing values, handling NULLs, and creating staging tables. It also includes various SELECT statements to verify the integrity of the data throughout the process.

--- layoffs_data_cleaning.sql
+++ layoffs_data_cleaning.sql
@@ -5,3 +5,260 @@ Standardizing data
 Handling NULL values
 Converting dates
 Final cleaned table
+
+
+SELECT * 
+FROM layoffs;
+
+CREATE TABLE layoffs_staging 
+LIKE layoffs;
+
+INSERT layoffs_staging 
+SELECT * FROM layoffs;
+
+
+
+
+SELECT *
+FROM layoffs_staging
+;
+
+SELECT company, industry, total_laid_off,`date`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, industry, total_laid_off,`date`) AS row_num
+	FROM 
+		layoffs_staging;
+
+
+
+SELECT *
+FROM (
+	SELECT company, industry, total_laid_off,`date`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, industry, total_laid_off,`date`
+			) AS row_num
+	FROM 
+		layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1;
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Oda'
+;
+
+SELECT *
+FROM (
+	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1;
+
+
+WITH DELETE_CTE AS 
+(
+SELECT *
+FROM (
+	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1
+)
+DELETE
+FROM DELETE_CTE
+;
+
+
+WITH DELETE_CTE AS (
+	SELECT company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, 
+    ROW_NUMBER() OVER (PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+	FROM layoffs_staging
+)
+DELETE FROM layoffs_staging
+WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, row_num) IN (
+	SELECT company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, row_num
+	FROM DELETE_CTE
+) AND row_num > 1;
+
+
+ALTER TABLE layoffs_staging ADD row_num INT;
+
+
+SELECT *
+FROM layoffs_staging
+;
+
+CREATE TABLE `layoffs_staging2` (
+`company` text,
+`location`text,
+`industry`text,
+`total_laid_off` INT,
+`percentage_laid_off` text,
+`date` text,
+`stage`text,
+`country` text,
+`funds_raised_millions` int,
+row_num INT
+);
+
+INSERT INTO `layoffs_staging2`
+(`company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+`row_num`)
+SELECT `company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		layoffs_staging;
+
+
+DELETE FROM world_layoffs.layoffs_staging2
+WHERE row_num >= 2;
+
+
+SELECT * 
+FROM layoffs_staging2;
+
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Bally%';
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company LIKE 'airbnb%';
+
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
+
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY country;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country);
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY country;
+
+SELECT *
+FROM layoffs_staging2;
+
+--  use str to date to update this field
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+-- convert the data type properly
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+SELECT * 
+FROM layoffs_staging2;
