f775f30f0e8a98b9e2e269a50c3da4ebde98f898
Thrushitha-08 <nomulathrushitha6065@gmail.com>
2026-06-30 23:18:07 +0530

    Add multiple queries for layoffs analysis

--- layoffs_eda.sql
+++ layoffs_eda.sql
@@ -5,3 +5,59 @@ Layoffs by year
 Layoffs by stage
 Companies with 100% layoffs
 Top locations
+
+
+SELECT * 
+FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off),MAX(percentage_laid_off) 
+FROM layoffs_staging2;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1 
+ORDER BY funds_raised_millions DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company, total_laid_off
+FROM layoffs_staging2
+ORDER BY 2 DESC
+LIMIT 5;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+SELECT location, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(date), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 ASC;
+
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
